555a34b7a78d977cd287c64ae357a90a6d522966
RomixERR <romixerr@mail.ru>
2022-04-13 16:05:39 +0300

    Create Panel For Bye and Sell/ Script for List Panel

---
+++ Assets/Store/Scripts/StoreItemScript.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StoreItemScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [Header("Editable fields")]
+    public Sprite Icon;
+    public string NameTitle;
+    public int Cost;
+    public Color AvailableForPurchase;
+    public Color NotAvailableForPurchase;
+    public bool CanBye = true;
+    public bool CanSell = true;
+    [Header("Link fields")]
+    public Image LinkedImage;
+    public Text LinkedTextItemName;
+    public Text LinkedTextCost;
+
+    public bool NowCanBye = true;
+    public bool NowCanSell = true;
+    private Image Image;
+
+    void Start()
+    {
+        Image = GetComponent<Image>();
+        SetItemAtrributes();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NowCanBye) { Image.color = AvailableForPurchase; }
+        else { Image.color = NotAvailableForPurchase; }
+
+    }
+
+    void SetItemAtrributes()
+    {
+        LinkedImage.sprite = Icon;
+        LinkedTextItemName.text = NameTitle;
+        LinkedTextCost.text = "Cost: " + Cost.ToString() + "$";
+    }
+
+    public void OnPress()
+    {
+        if (NowCanBye) {
+            Debug.Log(NameTitle);
+        }
+    }
+
+
+
+}
